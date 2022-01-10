f1c4dc7fa675aecbd5472ce0d7027c3d1580b5ef
islay7 <islay@gmx.fr>
2022-01-11 00:04:39 +0100

    Projection for vector Layers. RasterLayerViewer

---
+++ Source/RasterLayerViewer.cpp
@@ -0,0 +1,235 @@
+//==============================================================================
+// RasterLayerViewer.cpp
+//
+// Created: 10 Jan 2022 9:58:48pm
+// Author:  FBecirspahic
+//==============================================================================
+
+#include "RasterLayerViewer.h"
+
+//==============================================================================
+// RasterLayerViewerModel : constructeur
+//==============================================================================
+RasterLayerViewerModel::RasterLayerViewerModel()
+{
+	m_Base = nullptr;
+	m_ActiveRow = m_ActiveColumn = -1;
+}
+
+//==============================================================================
+// Nombre de lignes de la table
+//==============================================================================
+int RasterLayerViewerModel::getNumRows()
+{
+	if (m_Base == nullptr)
+		return 0;
+	return m_Base->GetRasterLayerCount();
+}
+
+// This is overloaded from TableListBoxModel, and should fill in the background of the whole row
+void RasterLayerViewerModel::paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected)
+{
+	g.setColour(juce::Colours::lightblue);
+	if (rowIsSelected)
+		g.drawRect(g.getClipBounds());
+	g.setColour(juce::Colours::black);
+}
+
+void RasterLayerViewerModel::paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool rowIsSelected)
+{
+	if (m_Base == nullptr)
+		return;
+	if (rowNumber >= m_Base->GetRasterLayerCount())
+		return;
+	GeoBase::RasterLayer* geoLayer = m_Base->GetRasterLayer(rowNumber);
+	if (geoLayer == nullptr)
+		return;
+
+	switch (columnId) {
+	case Column::Name:
+		g.drawText(juce::String(geoLayer->Name()), 0, 0, width, height, juce::Justification::centredLeft);
+		break;
+	case Column::Opacity:
+		g.drawText(juce::String(geoLayer->Opacity()*100), 0, 0, width, height, juce::Justification::centred);
+		break;
+	/*
+	case Column::PenColour:// Pen
+		g.setColour(juce::Colour(geoLayer->m_Repres.PenColor));
+		g.fillRect(0, 0, width, height);
+		break;
+	case Column::FillColour:// brush
+		g.setColour(juce::Colour(geoLayer->m_Repres.FillColor));
+		g.fillRect(0, 0, width, height);
+		break;
+		*/
+	}
+}
+
+//==============================================================================
+// Clic dans une cellule
+//==============================================================================
+void RasterLayerViewerModel::cellClicked(int rowNumber, int columnId, const juce::MouseEvent& event)
+{
+	if (m_Base == nullptr)
+		return;
+	if (rowNumber >= m_Base->GetRasterLayerCount())
+		return;
+	GeoBase::RasterLayer* geoLayer = m_Base->GetRasterLayer(rowNumber);
+	m_ActiveRow = rowNumber;
+	m_ActiveColumn = columnId;
+	juce::Rectangle<int> bounds;
+	bounds.setCentre(event.getMouseDownScreenPosition());
+	bounds.setWidth(1); bounds.setHeight(1);
+	/*
+	// Choix d'une couleur
+	if ((columnId == Column::PenColour) || (columnId == Column::FillColour)) {
+		auto colourSelector = std::make_unique<juce::ColourSelector>(juce::ColourSelector::showAlphaChannel
+			| juce::ColourSelector::showColourAtTop
+			| juce::ColourSelector::editableColour
+			| juce::ColourSelector::showSliders
+			| juce::ColourSelector::showColourspace);
+
+		colourSelector->setName("background");
+		colourSelector->setCurrentColour(juce::Colour(geoLayer->m_Repres.PenColor));
+		colourSelector->addChangeListener(this);
+		colourSelector->setColour(juce::ColourSelector::backgroundColourId, juce::Colours::transparentBlack);
+		colourSelector->setSize(400, 300);
+
+		juce::CallOutBox::launchAsynchronously(std::move(colourSelector), bounds, nullptr);
+		return;
+	} */
+	// Choix d'une epaisseur
+	if (columnId == Column::Opacity) {
+		auto opacitySelector = std::make_unique<juce::Slider>();
+		opacitySelector->setRange(0., 100., 1.);
+		opacitySelector->setValue(geoLayer->Opacity()*100.);
+		opacitySelector->setSliderStyle(juce::Slider::LinearHorizontal);
+		opacitySelector->setTextBoxStyle(juce::Slider::TextBoxLeft, false, 80, 20);
+		opacitySelector->setSize(200, 50);
+		opacitySelector->addListener(this);
+		juce::CallOutBox::launchAsynchronously(std::move(opacitySelector), bounds, nullptr);
+		return;
+	}
+}
+
+//==============================================================================
+// Drag&Drop des lignes pour changer l'ordre des layers
+//==============================================================================
+juce::var RasterLayerViewerModel::getDragSourceDescription(const juce::SparseSet<int>& selectedRows)
+{
+	juce::StringArray rows;
+	for (int i = 0; i < selectedRows.size(); i++)
+		rows.add(juce::String(selectedRows[i]));
+	return rows.joinIntoString(":");
+}
+
+//==============================================================================
+// LayerViewer : constructeur
+//==============================================================================
+void RasterLayerViewerModel::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+	if (m_Base == nullptr)
+		return;
+	if (m_ActiveRow >= m_Base->GetRasterLayerCount())
+		return;
+	GeoBase::RasterLayer* geoLayer = m_Base->GetRasterLayer(m_ActiveRow);
+	/*
+	// Choix d'une couleur
+	if ((m_ActiveColumn == Column::PenColour) || (m_ActiveColumn == Column::FillColour)) {
+		if (auto* cs = dynamic_cast<juce::ColourSelector*> (source)) {
+			GUInt32 color = cs->getCurrentColour().getARGB();
+			if (m_ActiveColumn == Column::PenColour)
+				geoLayer->m_Repres.PenColor = color;
+			if (m_ActiveColumn == Column::FillColour)
+				geoLayer->m_Repres.FillColor = color;
+			sendActionMessage("UpdateRepres");
+		}
+	}
+	*/
+}
+
+//==============================================================================
+// Changement de valeur des sliders
+//==============================================================================
+void RasterLayerViewerModel::sliderValueChanged(juce::Slider* slider)
+{
+	if (m_Base == nullptr)
+		return;
+	if (m_ActiveRow >= m_Base->GetRasterLayerCount())
+		return;
+	GeoBase::RasterLayer* geoLayer = m_Base->GetRasterLayer(m_ActiveRow);
+
+	// Choix d'une epaisseur
+	if (m_ActiveColumn == Column::Opacity) {
+		geoLayer->Opacity(slider->getValue() * 0.01);
+		sendActionMessage("UpdateRepres");
+	}
+}
+
+//==============================================================================
+// RasterLayerViewer : constructeur
+//==============================================================================
+RasterLayerViewer::RasterLayerViewer()
+{
+	m_Base = nullptr;
+	setName("Raster Layers");
+	m_Model.addActionListener(this);
+	// Bordure
+	m_Table.setColour(juce::ListBox::outlineColourId, juce::Colours::grey);
+	m_Table.setOutlineThickness(1);
+	// Ajout des colonnes
+	m_Table.getHeader().addColumn(juce::translate("Name"), RasterLayerViewerModel::Column::Name, 200);
+	m_Table.getHeader().addColumn(juce::translate("Opacity"), RasterLayerViewerModel::Column::Opacity, 50);
+	m_Table.getHeader().addColumn(juce::translate("Pen"), RasterLayerViewerModel::Column::PenColour, 50);
+	m_Table.getHeader().addColumn(juce::translate("Brush"), RasterLayerViewerModel::Column::FillColour, 50);
+	m_Table.setSize(352, 200);
+	m_Table.setModel(&m_Model);
+	addAndMakeVisible(m_Table);
+}
+
+//==============================================================================
+// LayerViewer : mise a jour du nom des colonnes (pour la traduction)
+//==============================================================================
+void RasterLayerViewer::UpdateColumnName()
+{
+	m_Table.getHeader().setColumnName(RasterLayerViewerModel::Column::Name, juce::translate("Name"));
+	m_Table.getHeader().setColumnName(RasterLayerViewerModel::Column::Opacity, juce::translate("Opacity"));
+	m_Table.getHeader().setColumnName(RasterLayerViewerModel::Column::PenColour, juce::translate("Pen"));
+	m_Table.getHeader().setColumnName(RasterLayerViewerModel::Column::FillColour, juce::translate("Brush"));
+}
+
+//==============================================================================
+// Gestion des actions
+//==============================================================================
+void RasterLayerViewer::actionListenerCallback(const juce::String& message)
+{
+	if (message == "UpdateRepres") {
+		repaint();
+	}
+}
+
+//==============================================================================
+// Drag&Drop
+//==============================================================================
+void RasterLayerViewer::itemDropped(const SourceDetails& details)
+{
+	juce::String message = details.description.toString();
+	juce::StringArray T;
+	T.addTokens(message, ":", "");
+	if (T.size() < 1)
+		return;
+	int i;
+	i = T[0].getIntValue();
+	int row = m_Table.getRowContainingPosition(details.localPosition.x, details.localPosition.y);
+	m_Base->ReorderRasterLayer(i, row);
+	//m_Table.updateContent();
+	//m_Table.repaint();
+	m_Model.sendActionMessage("UpdateRepres");
+}
+
+bool RasterLayerViewer::isInterestedInDragSource(const SourceDetails& details)
+{
+	if (details.sourceComponent.get() != &m_Table)
+		return false;
+	return true;
+}
---
+++ Source/RasterLayerViewer.h
@@ -0,0 +1,70 @@
+//==============================================================================
+// RasterLayerViewer.h
+//
+// Created: 10 Jan 2022 9:58:48pm
+// Author:  FBecirspahic
+//==============================================================================
+
+#pragma once
+#include <JuceHeader.h>
+#include "GeoBase.h"
+
+//==============================================================================
+// RasterLayerViewerModel : table pour montrer les proprietes des layers
+//==============================================================================
+class RasterLayerViewerModel : public juce::TableListBoxModel,
+	public juce::ChangeListener,
+	public juce::Slider::Listener,
+	public juce::ActionBroadcaster {
+public:
+	typedef enum { Name = 1, Opacity = 2, PenColour = 3, FillColour = 4 } Column;
+	RasterLayerViewerModel();
+
+	int getNumRows() override;
+	void paintRowBackground(juce::Graphics& g, int rowNumber, int width, int height, bool rowIsSelected) override;
+	void paintCell(juce::Graphics& g, int rowNumber, int columnId, int width, int height, bool /*rowIsSelected*/) override;
+	void cellClicked(int rowNumber, int columnId, const juce::MouseEvent&) override;
+	juce::var getDragSourceDescription(const juce::SparseSet<int>& selectedRows) override;
+
+	void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+	void sliderValueChanged(juce::Slider* slider) override;
+
+	void SetBase(GeoBase* base) { m_Base = base; }
+
+private:
+	GeoBase* m_Base;
+	int										m_ActiveRow;
+	int										m_ActiveColumn;
+};
+
+//==============================================================================
+// RasterLayerViewer : fenetre pour contenir
+//==============================================================================
+class RasterLayerViewer : public juce::Component,
+	public juce::ActionListener,
+	public juce::DragAndDropTarget,
+	public juce::DragAndDropContainer {
+public:
+	RasterLayerViewer();
+
+	void SetBase(GeoBase* base) { m_Base = base;  m_Model.SetBase(base); m_Table.updateContent(); }
+	void SetActionListener(juce::ActionListener* listener) { m_Model.addActionListener(listener); }
+	void UpdateColumnName();
+	void resized() override { auto b = getLocalBounds(); m_Table.setSize(b.getWidth(), b.getHeight()); }
+	// Gestion des actions
+	void actionListenerCallback(const juce::String& message) override;
+
+	// Drag&Drop
+	void itemDropped(const SourceDetails& details) override;
+	bool isInterestedInDragSource(const SourceDetails& details) override;
+	void itemDragEnter(const SourceDetails& ) override { ; }
+	void itemDragMove(const SourceDetails& ) override { ; }
+	void itemDragExit(const SourceDetails&) override { ; }
+
+private:
+	GeoBase* m_Base;
+	juce::TableListBox				m_Table;
+	RasterLayerViewerModel		m_Model;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RasterLayerViewer)
+};
